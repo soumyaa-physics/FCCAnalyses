297b0fb489545ca2bb73f8c5ea4b165ec3d61a0b
Maximilian Emanuel Goblirsch-Kolb <goblirsc@cern.ch>
2026-02-16 11:58:08 +0100

    support index-based selection

--- analyzers/dataframe/src/VertexingUtils.cc
+++ analyzers/dataframe/src/VertexingUtils.cc
@@ -1,6 +1,6 @@
 #include "FCCAnalyses/VertexingUtils.h"
 #include "FCCAnalyses/VertexFitterSimple.h"
-
+#include <set> 
 #include "TrkUtil.h" // from delphes
 
 namespace FCCAnalyses {
@@ -391,6 +391,24 @@ ROOT::VecOps::RVec<int> get_VerticesRecoParticlesInd(
   }
   
   return result;
+}  
+
+int getVertex_matching_recoParticles(const ROOT::VecOps::RVec<FCCAnalysesVertex > & vertices,  
+                                       const ROOT::VecOps::RVec<int> & recoParticleIndices,
+                                       const ROOT::VecOps::RVec<edm4hep::ReconstructedParticleData> &reco,
+                                       bool require_all){
+  std::set<int> indicesWeWant; 
+  indicesWeWant.insert(recoParticleIndices.begin(),recoParticleIndices.end()); 
+  // correct for "-1" representing missed tracks in the recoParticleIndices
+  int correctMissing = indicesWeWant.count(-1); 
+  for (int iVX = 0; iVX < vertices.size(); ++iVX){
+    auto vxParticleIndices = get_VertexRecoParticlesInd(vertices[iVX],reco); 
+    int nFound = std::count_if(vxParticleIndices.begin(), vxParticleIndices.end(),[&](int recoIndex){
+      return indicesWeWant.count(recoIndex);
+    }) ; 
+    if (require_all && nFound == indicesWeWant.size() - correctMissing || nFound == vxParticleIndices.size()) return iVX;
+  }
+  return -1; 
 }
 
 
